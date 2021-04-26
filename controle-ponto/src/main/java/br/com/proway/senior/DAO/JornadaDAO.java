f3c6aee8b5d086739ab906269ac4e961326d7b3d
SENIOR <SENIOR@proway.treina>
2021-04-26 17:45:45 -0300

    implementando testes e metodos

--- controle-ponto/src/main/java/br/com/proway/senior/DAO/JornadaDAO.java
+++ controle-ponto/src/main/java/br/com/proway/senior/DAO/JornadaDAO.java
@@ -1,5 +1,72 @@
 package br.com.proway.senior.DAO;
 
-public class JornadaDAO {
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import br.com.proway.senior.modelos.Ponto;
+
+public final class JornadaDAO {
+	private static JornadaDAO instance;
+	Ponto ponto = new Ponto();
+	public ArrayList<Ponto> pontos = new ArrayList<Ponto>();
 	
+	private JornadaDAO(Ponto ponto) {
+		this.ponto = ponto;
+	}
+	
+	public static JornadaDAO getInstance(Ponto ponto) {
+		if (instance == null) {
+			instance = new JornadaDAO(ponto);
+		}
+		return instance;
+	}
+	
+	public boolean cadastrarPonto(Ponto ponto) {
+		if(ponto != null) {
+			this.pontos.add(ponto);
+			return true;
+		}
+		return false;
+	}
+	
+	public ArrayList<Ponto> buscarTodos() {
+		return this.pontos;
+	}
+	
+	public ArrayList<Ponto> buscarTodosPontosPorPessoaId(int id) {
+		ArrayList<Ponto> listaPontosBuscada = new ArrayList<Ponto>();
+		for(Ponto ponto : pontos) {
+			if(ponto.getIdPessoa() == id) {
+				listaPontosBuscada.add(ponto);
+			}
+		}
+		return listaPontosBuscada;
+	}
+	
+	public ArrayList<Ponto> buscarTodosPontosData(LocalDate data) {
+		ArrayList<Ponto> listaPontosBuscada = new ArrayList<Ponto>();
+		for(Ponto ponto : pontos) {
+			if(ponto.getDataPonto() == data) {
+				listaPontosBuscada.add(ponto);
+			}
+		}
+		return listaPontosBuscada;
+	}
+	
+	public Ponto buscarPontoPorId(int id) {
+		for(Ponto ponto : pontos) {
+			if(ponto.getIdPonto() == id) {
+				return ponto;
+			}
+		}
+		return null;
+	}
+	
+	public void alterarPonto(int id) {
+		for(Ponto ponto : pontos) {
+			if(ponto.getIdPonto() == id) {
+
+			}
+		}
+	}
 }
---
+++ controle-ponto/src/main/java/br/com/proway/senior/controller/ControllerJornada.java
@@ -0,0 +1,5 @@
+package br.com.proway.senior.controller;
+
+public class ControllerJornada {
+	
+}
