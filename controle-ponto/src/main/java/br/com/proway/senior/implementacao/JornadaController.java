6e2483c1a1a8bb01989d3ae808debd62e02e1f0a
Leonardo F Silva <felipeleao217@gmail.com>
2021-05-03 11:07:37 -0300

    arrumando jornada

---
+++ controle-ponto/src/main/java/br/com/proway/senior/implementacao/JornadaController.java
@@ -0,0 +1,12 @@
+package br.com.proway.senior.implementacao;
+
+import br.com.proway.senior.DAO.JornadaDAO;
+
+public class JornadaController {
+	
+	public void cadastrarJornada(int idPessoa ) {
+		JornadaDAO jornadaDB = JornadaDAO.getInstance();
+		
+		
+	}
+}
--- controle-ponto/src/main/java/br/com/proway/senior/modelos/Jornada.java
+++ controle-ponto/src/main/java/br/com/proway/senior/modelos/Jornada.java
@@ -1,48 +1,42 @@
 package br.com.proway.senior.modelos;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class Jornada {
-	private Integer idJornada;
-	private Integer idPessoa;
-	private boolean aberta = true;
-	private ArrayList<Ponto> pontos;
-
-	public Integer getIdPessoa() {
+	private int id;
+	private int idPessoa;
+	private LocalDate data;
+	private int idTurno;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdPessoa() {
 		return idPessoa;
 	}
-
-	public void setIdPessoa(Integer idPessoa) {
+	public void setIdPessoa(int idPessoa) {
 		this.idPessoa = idPessoa;
 	}
-
-	public Integer getId() {
-		return idJornada;
+	public LocalDate getData() {
+		return data;
 	}
-
-	public void setId(Integer id) {
-		this.idJornada = id;
+	public void setData(LocalDate data) {
+		this.data = data;
 	}
-
-	public ArrayList<Ponto> getPontos() {
-		return pontos;
+	public int getIdTurno() {
+		return idTurno;
 	}
-
-	public void setPontos(ArrayList<Ponto> pontos) {
-		this.pontos = pontos;
+	public void setIdTurno(int idTurno) {
+		this.idTurno = idTurno;
 	}
-
-	public boolean isAberta() {
-		return aberta;
-	}
-
-	public void setAberta(boolean aberta) {
-		this.aberta = aberta;
-	}
-
-	@Override
-	public String toString() {
-		return "Jornada [idJornada=" + idJornada + ", idPessoa=" + idPessoa + ", aberta=" + aberta + ", pontos="
-				+ pontos + "]";
+	public Jornada(int id, int idPessoa, LocalDate data, int idTurno) {
+		super();
+		this.id = id;
+		this.idPessoa = idPessoa;
+		this.data = data;
+		this.idTurno = idTurno;
 	}
 }
