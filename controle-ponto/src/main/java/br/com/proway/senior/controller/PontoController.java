1e15c8c006cb063c97671fcbc3b69f9c7f46a668
Lucas Walim <lucaswalim@hotmail.com>
2021-05-11 08:41:23 -0300

    Sprint5 alterada

--- controle-ponto/src/main/java/br/com/proway/senior/controller/PontoController.java
+++ controle-ponto/src/main/java/br/com/proway/senior/controller/PontoController.java
@@ -1,5 +1,22 @@
 package br.com.proway.senior.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import br.com.proway.senior.model.Ponto;
+
 public class PontoController {
+	public static void main(String[] args) {
+		Ponto ponto = new Ponto(1, LocalDateTime.now());
+		
+		LocalDateTime registro = ponto.getMomentoPonto();
+		
+		//System.out.println(ponto.getMomentoPonto());
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY/MM/dd HH:mm");
+		
+		System.out.println(registro.format(formatter));
+		
+		
 	
+	}
 }
