82b1fb0df8ae77b49511f901e56c3c91586ff19b
vitor andré gehrke <vitor.gehrke@gmail.com>
2021-05-17 13:42:38 -0300

    inicio dos testes do PontoController

--- controle-ponto/src/test/java/br/com/proway/senior/controller/PontoControllerTest.java
+++ controle-ponto/src/test/java/br/com/proway/senior/controller/PontoControllerTest.java
@@ -1,123 +1,80 @@
 package br.com.proway.senior.controller;
 
-import java.time.LocalDate;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
-import org.junit.Test;
+import org.hibernate.Session;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import br.com.proway.senior.DAO.PontoDAO;
-import br.com.proway.senior.model.Jornada;
+import br.com.proway.senior.dbpersistence.DBConnection;
 import br.com.proway.senior.model.Ponto;
-import junit.framework.TestCase;
 
-public class PontoControllerTest extends TestCase {
-//<<<<<<< HEAD
-////
-//=======
-//
-//>>>>>>> 713098d011c3b0ebf01481ec19485836a6b57605
-////	@Test
-////	public void testDiferencaHoras() {
-////		// Cenário
-////		Ponto registro1 = new Ponto(1, LocalDateTime.of(2021, 5, 11, 8, 0));
-////		Ponto registro2 = new Ponto(1, LocalDateTime.of(2021, 5, 11, 8, 0));
-////		Ponto registro3 = new Ponto(1, LocalDateTime.of(2021, 5, 11, 9, 0));
-////		Ponto registro4 = new Ponto(1, LocalDateTime.of(2021, 5, 11, 9, 10));
-////		Long dif1 = ChronoUnit.MINUTES.between(registro1.getMomentoPonto(), registro2.getMomentoPonto());
-////		Long dif2 = ChronoUnit.MINUTES.between(registro3.getMomentoPonto(), registro4.getMomentoPonto());
-////		Long htdia01 = dif1 + dif2;
-////
-////		Ponto registro5 = new Ponto(1, LocalDateTime.of(2021, 5, 12, 8, 0));
-////		Ponto registro6 = new Ponto(1, LocalDateTime.of(2021, 5, 12, 9, 0));
-////		Ponto registro7 = new Ponto(1, LocalDateTime.of(2021, 5, 12, 9, 5));
-////		Ponto registro8 = new Ponto(1, LocalDateTime.of(2021, 5, 12, 9, 6));
-////		Long dif3 = ChronoUnit.MINUTES.between(registro5.getMomentoPonto(), registro6.getMomentoPonto());
-////		Long dif4 = ChronoUnit.MINUTES.between(registro7.getMomentoPonto(), registro8.getMomentoPonto());
-////		Long htdia02 = dif3 + dif4;
-////
-//////        //Ação a ser testada
-//////        System.out.println(htdia01);
-//////        System.out.println(htdia02);
-//////        System.out.println("Saldo: " + (htdia01 + htdia02));
-////
-////	}
-////
-////	@Test
-////	public void testCreate() throws Exception {
-////		PontoDAO pontoDao = PontoDAO.getInstance();
-////		PontoController controller = new PontoController();
-////
-////		Jornada jornada = new Jornada(1, 13, LocalDate.of(2021, 05, 11), 7);
-////		controller.create(jornada);
-////	}
-////
-////	@Test
-////	public void testCreate2() throws Exception {
-////		PontoDAO pontoDao = PontoDAO.getInstance();
-////		PontoController controller = new PontoController();
-////
-////		Jornada jornada = new Jornada(1, 3, LocalDate.of(2021, 05, 9), 7);
-////		controller.create(jornada);
-////
-////	}
-////
-////	@Test
-////    public void testReadByJornada() throws Exception {
-////    	PontoDAO pontoDAO = PontoDAO.getInstance();
-////    	PontoController controller = new PontoController();
-////    	int idJornada = 114;
-//<<<<<<< HEAD
-////
-//=======
-////    	
-//>>>>>>> 713098d011c3b0ebf01481ec19485836a6b57605
-////    	ArrayList<Ponto> registros = controller.readByIdJornada(idJornada);
-////    	for(Ponto ponto : registros) {
-////    		System.out.println(ponto);
-////    		assertSame(idJornada, ponto.getIdJornada());
-////    	}
-////    }
-//<<<<<<< HEAD
-////
-//=======
-////	
-//>>>>>>> 713098d011c3b0ebf01481ec19485836a6b57605
-////	@Test
-////	public void testReadAll() {
-////		PontoDAO pontoDAO = PontoDAO.getInstance();
-////    	PontoController controller = new PontoController();
-//<<<<<<< HEAD
-////
-//=======
-////    	
-//>>>>>>> 713098d011c3b0ebf01481ec19485836a6b57605
-////    	ArrayList<Ponto> lista = controller.readAll();
-////    	for(Ponto item : lista) {
-////    		System.out.println(item);
-////    	}
-//<<<<<<< HEAD
-////
-////    	assertEquals(9, lista.size());
-////	}
-////
-//=======
-////    	
-////    	assertEquals(9, lista.size());
-////	}
-////	
-//>>>>>>> 713098d011c3b0ebf01481ec19485836a6b57605
-////	@Test
-////	public void testDelete() {
-////		PontoDAO pontoDAO = PontoDAO.getInstance();
-////    	PontoController controller = new PontoController();
-//<<<<<<< HEAD
-////
-//=======
-////    	
-//>>>>>>> 713098d011c3b0ebf01481ec19485836a6b57605
-////    	controller.delete(7);
-////    	assertEquals(9, controller.readAll().size());
-////	}
+class PontoControllerTest {
+
+	static Session session;
+	static PontoController pontoController;
+	static PontoDAO pdao;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		session = DBConnection.getSession();
+		pontoController = new PontoController(session);
+		pdao = new PontoDAO(session);
+
+	}
+
+	@Test
+	void testInsert() throws Exception {
+		Ponto ponto = new Ponto(null, LocalDateTime.of(1997, 5, 13, 23, 59));
+		pontoController.insert(ponto);
+		ArrayList<Ponto> listaPontos = pontoController.getAll();
+		for (int i = 0; i < listaPontos.size(); i++) {
+			if (listaPontos.get(i).equals(ponto)) {
+				assertEquals(ponto, pontoController.getAll().get(i));
+			}
+		}
+	}
+	
+	@Test
+	void testInsertNotEquals() throws Exception {
+		Ponto ponto = new Ponto(null, LocalDateTime.of(1997, 5, 13, 23, 59));
+		pontoController.insert(ponto);
+		ArrayList<Ponto> listaPontos = pontoController.getAll();
+		for (int i = 0; i < listaPontos.size(); i++) {
+			if (!listaPontos.get(i).equals(ponto)) {
+				assertNotEquals(ponto, pontoController.getAll().get(i));
+			}
+		}
+	}
+	
+	@Test
+	void testGet() throws Exception {
+		assertNotNull(pontoController.get(19));
+	}
+	
+	@Test
+	void testGetFail() throws Exception {
+		assertNotNull(pontoController.get(-22));
+//		assertTh
+	}
+
+	@Test
+	void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
 }
