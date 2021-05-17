7be7c8cdde5f954a59a8f9184f9276e676c34278
samuelevi87 <samuel.levi.alves@outlook.com>
2021-05-17 16:11:14 -0300

    JornadaControllerTest com 90% de cobertura.

--- controle-ponto/src/test/java/br/com/proway/senior/controller/JornadaControllerTest.java
+++ controle-ponto/src/test/java/br/com/proway/senior/controller/JornadaControllerTest.java
@@ -8,36 +8,29 @@ import br.com.proway.senior.model.Jornada;
 import br.com.proway.senior.model.PessoaDoPonto;
 import br.com.proway.senior.model.Turno;
 import org.hibernate.Session;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 class JornadaControllerTest {
-    static Session session;
-    static JornadaController controller;
-    static JornadaDAO dao;
+    static Session session = DBConnection.getSession();
+    static JornadaController controller = new JornadaController(session);
+    static JornadaDAO dao = JornadaDAO.getInstance(session);
     static PessoaDoPonto pessoa;
-    static PessoaDAO pessoaDAO;
+    static PessoaDAO pessoaDAO = PessoaDAO.getInstance(session);
     static Turno turno;
-    static TurnoDAO turnoDAO;
+    static TurnoDAO turnoDAO = TurnoDAO.getInstance(session);
 
-    @BeforeAll
-    static void setUp() throws Exception {
-        session = DBConnection.getSession();
-        controller = new JornadaController(session);
-        dao = JornadaDAO.getInstance(session);
-        pessoaDAO.insert(new PessoaDoPonto(95));
-        turno = new Turno(99, LocalTime.of(8, 10), LocalTime.of(12, 10),
-                "Teste");
-        turnoDAO.create(turno);
-    }
 
     @Test
     void testInserindoUmaJornadaAtravesDaController() {
-        Jornada jornada = new Jornada(null, LocalDate.of(2020, 3, 3),
+        pessoa = new PessoaDoPonto(172);
+        pessoaDAO.insert(pessoa);
+        turno = new Turno(173, LocalTime.now(), LocalTime.now().plusHours(7), "Turno Test");
+        turnoDAO.insert(turno);
+        Jornada jornada = new Jornada(174, LocalDate.of(2020, 3, 3),
                 pessoa, turno);
         int tamanhoInicial = dao.getAll().size();
         controller.insert(jornada);
@@ -47,14 +40,37 @@ class JornadaControllerTest {
     }
 
     @Test
-    void get() {
+    void testGet() throws Exception {
+        pessoa = new PessoaDoPonto(26);
+        pessoaDAO.insert(pessoa);
+        turno = new Turno(999, LocalTime.now(), LocalTime.now().plusHours(7),
+                "Turno Test");
+        turnoDAO.insert(turno);
+        Jornada jornada = new Jornada(25, LocalDate.of(2555, 12, 29),
+                pessoa, turno);
+        controller.insert(jornada);
+
+        Assertions.assertEquals(2555, controller.get(26).getData().getYear());
     }
 
     @Test
-    void getAll() {
+    void testGetAll() {
+        Assertions.assertNotNull(controller.getAll());
     }
 
     @Test
-    void delete() {
+    void testDelete() {
+        pessoa = new PessoaDoPonto(26);
+        pessoaDAO.insert(pessoa);
+        turno = new Turno(999, LocalTime.now(), LocalTime.now().plusHours(7),
+                "Turno Test");
+        turnoDAO.insert(turno);
+        Jornada jornada = new Jornada(25, LocalDate.of(2555, 12, 29),
+                pessoa, turno);
+        controller.insert(jornada);
+        int tamanhoInicial = controller.getAll().size();
+        controller.delete(jornada);
+
+        Assertions.assertEquals(tamanhoInicial - 1, controller.getAll().size());
     }
 }
