2e18f9bc718600957970d74dc41d6634bd2aa989
Vanderlei Kleinschmidt <vanderleik@yahoo.com.br>
2021-05-17 13:36:59 -0300

    criacao da jornada controller e documentacao

--- controle-ponto/src/main/java/br/com/proway/senior/controller/JornadaController.java
+++ controle-ponto/src/main/java/br/com/proway/senior/controller/JornadaController.java
@@ -3,90 +3,75 @@ package br.com.proway.senior.controller;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+import org.hibernate.Session;
+
 import br.com.proway.senior.DAO.JornadaDAO;
 import br.com.proway.senior.DAO.PontoDAO;
+import br.com.proway.senior.model.Jornada;
 import br.com.proway.senior.model.interfaces.IPessoa;
 
+/**
+ * Classe responsável por receber as requisições, tratá-las e encaminhar para o
+ * DAO.
+ * 
+ * @author Vanderlei Kleinschmidt
+ * @author Samuel Levi
+ * @version sprint5
+ */
+
 public class JornadaController {
-//
-//	/**
-//	 * Verifica se a jornada aberta
-//	 *
-//	 * Caso a jornada esteja aberta retorna true
-//	 *
-//	 * @param pessoa
-//	 * @return true || false
-//	 */
-//	public boolean verificaJornadaAberta(IPessoa pessoa) {
-//		JornadaDAO jornadaDAO = JornadaDAO.getInstance();
-//		String dataUsuario = jornadaDAO.readByIdPessoa(pessoa).get(2);
-//
-//		if (dataUsuario.equals(LocalDate.now().toString()))
-//			return true;
-//		else
-//			return false;
-//	}
-//
-//	/**
-//	 * Dispara alerta de quantos pontos bateu
-//	 *
-//	 * Nao foi implementado
-//	 *
-//	 */
-//	public void pontosBatidos() {}
-//
-//	/**
-//	 * Quando sair do padr�o de batidas
-//	 *
-//	 * Sera disparado uma mensagem
-//	 *
-//	 * N�o foi implementado
-//	 *
-//	 */
-//	public void padraoBatidaPonto() {}
-//
-//	/**
-//	 * Este método fará o calculo das horas faltas.
-//	 *
-//	 * Pega as horas de acordo com o turno que trabalha.
-//	 *
-//	 * Faz a diferença, caso negativo retorna horas faltas,
-//	 * caso >= 0 retorna null.
-//	 *
-//	 * @author Tharlys Dias
-//	 * @author Vanderlei Kleinschmidt
-//	 * @author Vitor Gehrke
-//	 * @return horasFaltas
-//	 */
-//	public Double horasFalta(ArrayList<String> pontos) {
-//		Double horasFaltas = null;
-//		PontoDAO db = PontoDAO.getInstance();
-//
-//		for (int i = 0; i < db.toString().length(); i++) {
-//			//if () {
-//
-//			//}
-//		}
-//		return horasFaltas;
-//	}
-//
-//	/**
-//	 * Calcula as horas trabalhadas na jornada.
-//	 * Este método receb um número inteiro, correspondente à jornada que será
-//	 * calculada. Usará o array retornado pelo método que obtém a lista de
-//	 * pontos por jornada. Atrávés do Chrono Between retornará o valor em
-//	 * minutos da diferença entre a 1ª e 2ª batida e entre a 3ª e 4ª batida
-//	 * de ponto.
-//	 * @param index id da jornada a ser calculada
-//	 * @return tempoTrabalhado em minutos.
-//	 */
-////	public int tempoTrabalhado (int index){
-////		PontoController pontoController = new PontoController();
-////		//pontoController.
-////
-////
-////	}
 
+	private JornadaDAO dao;
+
+	/**
+	 * Construtor que instancia uma controller fazendo a integração com o
+	 * {@link JornadaDAO}.
+	 * 
+	 * @param session recebida como parâmetro.
+	 */
+	public JornadaController(Session session) {
+		this.dao = JornadaDAO.getInstance(session);
+
+	}
+
+	/**
+	 * Método que recebe um objeto {@link Jornada} e insere no banco.
+	 * 
+	 * @param jornadaASerInserida
+	 */
+	public void insert(Jornada jornadaASerInserida) {
+		dao.insert(jornadaASerInserida);
+	}
+
+	/**
+	 * Método que retorna uma {@link Jornada} pelo Id.
+	 * 
+	 * @param index Id da jornada a ser buscada.
+	 * @return retorna o objeto Jornada.
+	 * @throws Exception
+	 */
+	public Jornada get(int index) throws Exception {
+		if (dao.get(index) != null) {
+			return dao.get(index);
+		}
+		throw new Exception("Id inexistente");
+	}
+
+	/**
+	 * Método que retorna todas as {@link Jornada}.
+	 * 
+	 * @return ArrayList de Jornadas
+	 */
+	public ArrayList<Jornada> getAll() {
+		return (ArrayList<Jornada>) dao.getAll();
+	}
+
+	/**
+	 * Método que deleta uma jornada.
+	 * 
+	 * @param jornadaASerDeletada
+	 */
+	public void delete(Jornada jornadaASerDeletada) {
+		dao.delete(jornadaASerDeletada);
+	}
 }
-
-	
