8f3d79abba0a18c0b59c5c0338d0b2f24e3ae152
samuelevi87 <samuel.levi.alves@outlook.com>
2021-05-18 11:15:22 -0300

    Cálculo de tempo e exibição em formato de horas cheias.

--- controle-ponto/src/main/java/br/com/proway/senior/DAO/JornadaDAO.java
+++ controle-ponto/src/main/java/br/com/proway/senior/DAO/JornadaDAO.java
@@ -1,7 +1,6 @@
 package br.com.proway.senior.DAO;
 
 import br.com.proway.senior.model.Jornada;
-import br.com.proway.senior.model.Ponto;
 import br.com.proway.senior.model.interfaces.IPessoa;
 import br.com.proway.senior.utils.ICRUD;
 import org.hibernate.Session;
@@ -11,7 +10,10 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Root;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @version @Sprint5
@@ -77,7 +79,6 @@ public final class JornadaDAO implements ICRUD<Jornada> {
             e.getMessage();
         }
     }
-
 	/**
 	 * Recebe um inteiro que referencia o Id do ponto a ser recebido.
 	 *
@@ -184,5 +185,61 @@ public final class JornadaDAO implements ICRUD<Jornada> {
         criteria.from(Jornada.class);
         List<Jornada> listaTodasJornadas = session.createQuery(criteria).getResultList();
         return listaTodasJornadas;
+    }
+
+    public long tempoEntreRegistros(LocalDateTime registroInicial,
+                                    LocalDateTime registroFinal) {
+        return ChronoUnit.MINUTES.between(registroInicial, registroFinal);
+    }
+    /**
+     * Retorna o tempo formatado baseado no tempo informado.
+     * Exibe até a ordem de grandeza de dias.
+     * @param quantidade Long Quantidade do tempo
+     * @param unidadeTempo {@link TimeUnit} É a definição da ordem de
+     *                                     grandeza da quantidade de tempo.
+     * @version Sprint5
+     * @author Lucas Walim <lucas.walim@senior.com.br>
+     * @author Samuel Levi <samuel.levi@senior.com.br>
+     * @author Vanderlei Kleinschmidt <vanderlei.klein@senior.com.br>
+     * https://stackoverflow.com/questions/5387371/how-to-convert-minutes-to-hours-and-minutes-hhmm-in-java
+     */
+    public String minutosParaTempoTotal(long quantidade, TimeUnit unidadeTempo) {
+        long dia = unidadeTempo.toDays(quantidade);
+        long hora = unidadeTempo.toHours(quantidade) % 24;
+        long minuto = unidadeTempo.toMinutes(quantidade) % 60;
+        long segundo = unidadeTempo.toSeconds(quantidade) % 60;
+        if (dia > 0) {
+            return String.format("%d dia(s) %02d:%02d:%02d", dia, hora, minuto,
+                    segundo);
+        } else if (hora > 0) {
+            return String.format("%d:%02d:%02d", hora, minuto, segundo);
+        } else if (minuto > 0) {
+            return String.format("%d:%02d", minuto, segundo);
+        } else {
+            return String.format("%02d", segundo);
+        }
+    }
+    /**
+     * Retorna o tempo formatado baseado no tempo informado.
+     * Exibe apenas até a ordem de grandeza das horas.
+     * @param quantidade Long Quantidade do tempo
+     * @param unidadeTempo {@link TimeUnit} É a definição da ordem de
+     *                                     grandeza da quantidade de tempo.
+     * @version Sprint5
+     * @author Lucas Walim <lucas.walim@senior.com.br>
+     * @author Samuel Levi <samuel.levi@senior.com.br>
+     * @author Vanderlei Kleinschmidt <vanderlei.klein@senior.com.br>
+     */
+    public String minutosParaTempoSemDias(long quantidade, TimeUnit unidadeTempo) {
+        long hora = unidadeTempo.toHours(quantidade);
+        long minuto = unidadeTempo.toMinutes(quantidade) % 60;
+        long segundo = unidadeTempo.toSeconds(quantidade) % 60;
+        if (hora > 0) {
+            return String.format("%02d:%02d:%02d", hora, minuto, segundo);
+        } else if (minuto > 0) {
+            return String.format("%d:%02d", minuto, segundo);
+        } else {
+            return String.format("%02d", segundo);
+        }
     }
 }
--- controle-ponto/src/test/java/br/com/proway/senior/DAO/JornadaDAOTest.java
+++ controle-ponto/src/test/java/br/com/proway/senior/DAO/JornadaDAOTest.java
@@ -1,91 +1,116 @@
 package br.com.proway.senior.DAO;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-
-import org.hibernate.Session;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
 import br.com.proway.senior.dbpersistence.DBConnection;
 import br.com.proway.senior.model.Jornada;
 import br.com.proway.senior.model.Turno;
+import org.hibernate.Session;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JornadaDAOTest {
 
-	static Session session;
-	static JornadaDAO jornadaDao;
-	static PessoaDAO pessoaDao;
-	static TurnoDAO turnoDao;
+    static Session session;
+    static JornadaDAO jornadaDao;
+    static PessoaDAO pessoaDao;
+    static TurnoDAO turnoDao;
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		session = DBConnection.getSession();
-		jornadaDao = JornadaDAO.getInstance(session);
-		pessoaDao = PessoaDAO.getInstance(session);
-		turnoDao = TurnoDAO.getInstance(session);
-	}
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        session = DBConnection.getSession();
+        jornadaDao = JornadaDAO.getInstance(session);
+        pessoaDao = PessoaDAO.getInstance(session);
+        turnoDao = TurnoDAO.getInstance(session);
+    }
 
-	@Test
-	void testCreate() {
-		Jornada jornada = new Jornada();
-		jornada.setData(LocalDate.of(2333, 8, 24));
-		jornada.setPessoa(pessoaDao.get(119));
-		jornada.setTurno(turnoDao.get(120));
-		jornadaDao.insert(jornada);
+    @Test
+    void testCreate() {
+        Jornada jornada = new Jornada();
+        jornada.setData(LocalDate.of(2333, 8, 24));
+        jornada.setPessoa(pessoaDao.get(119));
+        jornada.setTurno(turnoDao.get(120));
+        jornadaDao.insert(jornada);
 
-		assertNotNull(jornada);
-	}
+        assertNotNull(jornada);
+    }
 
-	@Test
-	void testRead() {
-		assertEquals(36, jornadaDao.get(36).getId());
-	}
+    @Test
+    void testRead() {
+        assertEquals(35, jornadaDao.get(35).getId());
+    }
 
-	@Test
-	void testPegaTodasAsJornadasPorIdPessoa() {
-		pessoaDao.get(114);
-		ArrayList<Jornada> listaJornadas = (ArrayList) jornadaDao.readByIdPessoa(pessoaDao.get(114));
-		System.out.println("AQUI AQUI AQUI AQUI AQUI AQUI\\n\n\n\n\n\n\n\n");
-		System.out.println(jornadaDao.readByIdPessoa(pessoaDao.get(114)));
-		// assertNotNull(jornadaDao.readByIdPessoa(pessoaDao.find(114)));
+    @Test
+    void testPegaTodasAsJornadasPorIdPessoa() {
+        pessoaDao.get(114);
+        ArrayList<Jornada> listaJornadas = (ArrayList) jornadaDao.readByIdPessoa(pessoaDao.get(114));
+        System.out.println(jornadaDao.readByIdPessoa(pessoaDao.get(114)));
+        // assertNotNull(jornadaDao.readByIdPessoa(pessoaDao.find(114)));
 
-	}
+    }
 
-	@Test
-	void testDelete() {
+    @Test
+    void testDelete() {
+        Jornada jornada = new Jornada(19, LocalDate.of(3526, 12, 5),
+                pessoaDao.get(115), turnoDao.get(120));
+        jornadaDao.insert(jornada);
+        int inicio = jornadaDao.getAll().size();
+        jornadaDao.delete(jornada);
+        assertEquals(inicio - 1, jornadaDao.getAll().size());
+    }
 
-		Jornada jornada = new Jornada();
-		jornada.setId(125);
-		jornadaDao.delete(jornada);
+    @Test
+    void testUpdate() {
+        session.clear();
+        Turno turno = new Turno(0, LocalTime.now(), LocalTime.now().plusHours(4), "Turno 54");
+        turnoDao.insert(turno);
 
-		assertEquals(21, jornadaDao.getAll().size());
-	}
+        Jornada jornada2 = new Jornada(134, LocalDate.of(1999, 10, 5), pessoaDao.get(115), turno);
 
-	@Test
-	void testUpdate() {
-		session.clear();
-		Turno turno = new Turno(0, LocalTime.now(), LocalTime.now().plusHours(4), "Turno 54");
-		turnoDao.insert(turno);
+        jornadaDao.update(jornada2);
 
-		Jornada jornada2 = new Jornada(134, LocalDate.of(1999, 10, 5), pessoaDao.get(115), turno);
+    }
 
-		jornadaDao.update(jornada2);
+    @Test
+    void testReadAll() {
 
-	}
+        for (int i = 0; i < jornadaDao.getAll().size(); i++) {
+            if (jornadaDao.getAll().size() == i) {
+                assertEquals(i, jornadaDao.getAll().size());
+            }
+        }
 
-	@Test
-	void testReadAll() {
+    }
 
-		for (int i = 0; i < jornadaDao.getAll().size(); i++) {
-			if (jornadaDao.getAll().size() == i) {
-				assertEquals(i, jornadaDao.getAll().size());
-			}
-		}
+    @Test
+    void minutosParaTempoTotal() {
+        Long minutos = 1000l;
+        System.out.println(jornadaDao.minutosParaTempoTotal(minutos,
+                TimeUnit.MINUTES));
+    }
 
-	}
+    @Test
+    void minutosParaTempoSemDias() {
+        Long minutos = 1000l;
+        System.out.println(jornadaDao.minutosParaTempoSemDias(minutos, TimeUnit.MINUTES));
+
+    }
+
+    @Test
+    void tempoEntreRegistros() {
+        LocalDateTime inicio = LocalDateTime.now();
+        LocalDateTime fim = LocalDateTime.now().plusHours(3);
+        Assertions.assertEquals(180L, jornadaDao.tempoEntreRegistros(inicio,
+                fim));
+        Assertions.assertNotEquals(188L, jornadaDao.tempoEntreRegistros(inicio,
+                fim));
+    }
 }
