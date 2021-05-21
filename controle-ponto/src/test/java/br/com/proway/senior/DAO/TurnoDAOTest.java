e0c49fb61a958d972276600c625200d80868ba74
KenjiNishi <wk.nishizawa@gmail.com>
2021-05-21 09:31:10 -0300

    Todos os testes TurnoDAOTest passando

--- controle-ponto/src/test/java/br/com/proway/senior/DAO/TurnoDAOTest.java
+++ controle-ponto/src/test/java/br/com/proway/senior/DAO/TurnoDAOTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.time.LocalTime;
 
