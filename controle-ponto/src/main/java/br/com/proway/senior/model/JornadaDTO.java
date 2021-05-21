20965daf4c2a489343b1efea9bb9679bfed1f7d8
KenjiNishi <wk.nishizawa@gmail.com>
2021-05-21 16:51:13 -0300

    definidos o enum de estados da joranada, jornadaDTO com mais atributos relevantes

--- controle-ponto/src/main/java/br/com/proway/senior/model/JornadaDTO.java
+++ controle-ponto/src/main/java/br/com/proway/senior/model/JornadaDTO.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.proway.senior.controller.JornadaAPI;
+import br.com.proway.senior.enums.EstadosJornada;
+
 /**
  * DTO da entidade {@link Jornada} com os atributos relevantes para interface com outras
  * partes do sistema.
@@ -14,26 +17,50 @@ import java.util.List;
  *
  */
 public class JornadaDTO {
-	    
+	// Do objeto Jornada
     private int id;
     private LocalDate data; 
     private Turno turno;    
     public List<Ponto> listaPonto = new ArrayList<Ponto>();        
     private int idPessoa;
     
+    // Auxiliares proprias do DTO
+    public short dia;
+    public short mes;
+    public short ano;
+    
+    public long minutosTrabalhados;
+    public EstadosJornada estado;
+    
     /**
      * Construtor do {@link JornadaDTO}, recebe um objeto {@link Jornada} vindo do BD.
+     * 
+     * O objeto JornadaDTO compoe a estrutura de dados que vai para fora do nosso sistema.
      * 
      * @see DBConnection
      * 
      * @param Jornada : jornada
+     * @throws Exception 
      */
-    public JornadaDTO(Jornada jornada) {
+    public JornadaDTO(Jornada jornada) throws Exception {
         this.id = jornada.getId();
         this.data = jornada.getData();
         this.turno = jornada.getTurno();
         this.listaPonto = jornada.getListaPonto();
-        this.idPessoa = jornada.getIdPessoa();        
+        this.idPessoa = jornada.getIdPessoa(); 
+        
+        this.dia = (short) this.data.getDayOfMonth();
+        this.mes = (short) this.data.getMonthValue();
+        this.ano = (short) this.data.getYear();
+        
+        if(this.listaPonto.size() % 2 == 0) {
+        	this.minutosTrabalhados = JornadaAPI.calcularHorasTrabalhadas(jornada);
+        	this.estado = EstadosJornada.FECHADO;
+        }
+        else {
+        	this.minutosTrabalhados = 0;
+        	this.estado = EstadosJornada.ABERTO;
+        }
     }
 
 	public int getId() {
@@ -54,6 +81,26 @@ public class JornadaDTO {
 
 	public int getIdPessoa() {
 		return idPessoa;
-	}     
+	} 
+	
+	public short getDia() {
+		return dia;
+	}
+
+	public short getMes() {
+		return mes;
+	}
+
+	public short getAno() {
+		return ano;
+	}
+
+	public long getMinutosTrabalhados() {
+		return minutosTrabalhados;
+	}
+
+	public EstadosJornada getEstado() {
+		return estado;
+	}
 	
 }
