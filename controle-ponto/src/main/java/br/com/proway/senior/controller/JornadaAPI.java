6d18688bddf7a707d82f3bdd6924c6915d8445f5
KenjiNishi <wk.nishizawa@gmail.com>
2021-05-21 16:41:38 -0300

    calculo de periodo de tempo entre pontos agora tem tratamento de excessoes

--- controle-ponto/src/main/java/br/com/proway/senior/controller/JornadaAPI.java
+++ controle-ponto/src/main/java/br/com/proway/senior/controller/JornadaAPI.java
@@ -6,14 +6,32 @@ import java.util.List;
 import br.com.proway.senior.model.Jornada;
 import br.com.proway.senior.model.Ponto;
 import br.com.proway.senior.utils.FormatacaoDeTempo;
+import br.com.proway.senior.utils.Validadores;
 
 public class JornadaAPI {
-	
+	/**
+	 * Calcula o tempo trabalhado durante uma {@link Jornada} FECHADA (numero par de pontos)
+	 * e retorna esse valor em MINUTOS.
+	 * 
+	 * @param Jornada : jornada
+	 * @return long : Minutos trabalhados na Jornada.
+	 * 
+	 * @see FormatacaoDeTempo
+	 * @throws Exception
+	 */
 	public static long calcularHorasTrabalhadas(Jornada jornada) throws Exception {
-		long minutosTrabalhados = 0;
-		int pointer = 0;
+		if(Validadores.ehObjetoNulo(jornada)) {
+			throw(new Exception("Jornada esta nula!"));
+		}
 		
 		List<Ponto> pontosDaJornada = jornada.getListaPonto();
+		if (pontosDaJornada.size() % 2 != 0 ) {
+			throw(new Exception("A Jornada tem um numero impar de pontos e esta incompleta!"));
+		}
+		
+		long minutosTrabalhados = 0;
+		int pointer = 0;
+				
 		while (pointer < pontosDaJornada.size() - 1) {
 			LocalDateTime pontoAtual = pontosDaJornada.get(pointer).getMomentoPonto();
 			LocalDateTime proximo = pontosDaJornada.get(pointer+1).getMomentoPonto();
