182592dfe597964f0d0a2ea3413b0f94e595dd76
KenjiNishi <wk.nishizawa@gmail.com>
2021-05-24 17:44:29 -0300

    TODO: Terminar os testes do JornadaAPI, e REVISAR CONTROLLERS

--- controle-ponto/src/test/java/br/com/proway/senior/controller/JornadaAPITest.java
+++ controle-ponto/src/test/java/br/com/proway/senior/controller/JornadaAPITest.java
@@ -1,13 +1,28 @@
 package br.com.proway.senior.controller;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import br.com.proway.senior.dbpersistence.DBConnection;
+import br.com.proway.senior.model.Jornada;
+import br.com.proway.senior.model.Turno;
 
 class JornadaAPITest {
 
+	static JornadaAPI jornadaApi;
+	static Jornada jornada;
+	static JornadaController controllerJ;
+	static TurnoController controllerT;
 	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-
+		jornadaApi = new JornadaAPI();
+		JornadaController controllerJ = new JornadaController(DBConnection.getSession());
 	}
 	
 	// buscar jornada X
@@ -21,8 +36,17 @@ class JornadaAPITest {
 
 	
 	// buscar jornada da pessoa X no dia Y
-
 	
+	@Test
+	public void getJornadaTest() throws Exception {
+		Turno turno = new Turno(LocalTime.now(), LocalTime.now().plusHours(4), "Turno da noite");
+		controllerT.create(turno);
+		
+		jornada = new Jornada(LocalDate.of(2021, 5, 24), 12, turno);
+		Integer saida = controllerJ.create(jornada);
+		assertNotNull(jornadaApi.getJornada(saida));
+		
+		
+	}
 	
-
 }
