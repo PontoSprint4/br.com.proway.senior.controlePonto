6908dc31b17deb68f7dc0fe882b129d5b4c4085f
Thiago Barbieri <thiagoluizbarbieri@gmail.com>
2021-06-08 13:29:30 -0300

    Alterado htlm e o Css da Pagina de Historico de Jornada

--- frontend/MarcarPonto.js
+++ frontend/MarcarPonto.js
@@ -1,4 +1,6 @@
-
+const zeroFill = n => {
+    return ('0' + n).slice(-2);
+}
 
 // Cria intervalo
 const interval = setInterval(() => {
@@ -9,12 +11,26 @@ const interval = setInterval(() => {
    // const dataHora = zeroFill(now.getUTCDate()) + '/' + zeroFill((now.getMonth() + 1)) + '/' + now.getFullYear() + ' ' + zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
 
    // Pegando Hora e Minuto
-    const hora = (now.getHours()) + ':' + (now.getMinutes());
+    const hora =zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes());
 
-    
+    let mesesString;
+    switch (now.getMonth()+ 1) {
+        case 1: {mesesString = "Janeiro"; break;}
+        case 2: {mesesString = "Fevereiro"; break;}
+        case 3: {mesesString = "Março"; break;}
+        case 4: {mesesString = "Abril"; break;}
+        case 5: {mesesString = "Maio"; break;}
+        case 6: {mesesString = "Junho"; break;}
+        case 7: {mesesString = "Julho"; break;}
+        case 8: {mesesString = "Agosto"; break;}
+        case 9: {mesesString = "Setembro"; break;}
+        case 10: {mesesString = "Outubro"; break;}
+        case 11: {mesesString = "Novembro"; break;}
+        case 12: {mesesString = "Dezembro"; break;}
+    }
 
     // Pegando Dia, Mes e Ano
-    const data = now.getDay() +' de '+  +' de '+ now.getFullYear();
+    const data = zeroFill(now.getDay()) +' de '+ mesesString +' de '+ now.getFullYear();
 
     // Exibe na tela usando a div#data-hora
     document.getElementById('baterPonto-hora').innerHTML = hora;
