f82af1f0ddd6c2b5d04968751d5a1260aa642bef
enzomm <77839422+enzomm@users.noreply.github.com>
2021-06-09 15:19:41 -0300

    rotas-api

--- controle-de-ponto-client/src/app/app.module.ts
+++ controle-de-ponto-client/src/app/app.module.ts
@@ -5,17 +5,18 @@ import { AppComponent } from './app.component';
 import { PontoCrudListagemComponent } from './ponto-crud-listagem/ponto-crud-listagem.component';
 import { TurnoIndexComponent } from './turno-index/turno-index.component';
 import { TurnoDetailsComponent } from './turno-details/turno-details.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     PontoCrudListagemComponent,
     TurnoIndexComponent,
-    TurnoDetailsComponent,
     TurnoDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- controle-de-ponto-client/src/app/turno-index/turno-index.component.ts
+++ controle-de-ponto-client/src/app/turno-index/turno-index.component.ts
@@ -5,5 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './turno-index.component.html',
   styleUrls: ['./turno-index.component.css']
 })
-export class TurnoIndexComponent {
+export class TurnoIndexComponent { 
+  
+  public Turnos = [
+    {
+    id: 0,
+    nomeTurno: 'turno 0',
+    horaInicio: new Date(2021, 6, 8, 8, 0),
+    horaFim: new Date(2021, 6, 8, 18, 0),
+    minutosTrabalho: 600,
+    pessoasNoTurno: [0, 1, 2],
+  },  
+  {
+    id: 1,
+    nomeTurno: 'turno 1',
+    horaInicio: new Date(2021, 6, 8, 8, 0),
+    horaFim: new Date(2021, 6, 8, 18, 0),
+    minutosTrabalho: 600,
+    pessoasNoTurno: [0, 1, 2],
+  },
+  {
+    id: 2,
+    nomeTurno: 'turno 2',
+    horaInicio: new Date(2021, 6, 8, 8, 0),
+    horaFim: new Date(2021, 6, 8, 18, 0),
+    minutosTrabalho: 600,
+    pessoasNoTurno: [0, 1, 2],
+  }
+  ]
+
 }
