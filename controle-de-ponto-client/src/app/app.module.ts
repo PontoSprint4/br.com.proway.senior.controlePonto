2fc60f0160b201f698a3b67d9b313863126ec587
vitor gehrke <vitor.gehrke@gmail.com>
2021-06-09 10:45:53 -0300

    merge fix

--- controle-de-ponto-client/src/app/app.module.ts
+++ controle-de-ponto-client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PontoCrudListagemComponent } from './ponto-crud-listagem/ponto-crud-listagem.component';
 import { JornadaCrudListagemAdmComponent } from './jornada-crud-listagem-adm/jornada-crud-listagem-adm.component';
+import { JornadaListagemComponent } from './jornada-listagem/jornada-listagem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PontoCrudListagemComponent,
-    JornadaCrudListagemAdmComponent
+    JornadaCrudListagemAdmComponent,
+    JornadaListagemComponent,
   ],
   imports: [
     BrowserModule
